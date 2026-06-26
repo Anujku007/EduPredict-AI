d43e92fbdb3051ac615b3c192be30e80db6659b7
Anujku007 <anujkumar6205158@gmail.com>
2026-06-26 11:21:04 +0530

    model training done with 87% accuracy

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 
@@ -16,3 +18,37 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, Y_train, X_test, Y_test, models):
+    try:
+
+        report = {}
+
+        for model_name, model in models.items():
+
+            model.fit(X_train, Y_train)
+
+            Y_train_pred = model.predict(X_train)
+            Y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(Y_train, Y_train_pred)
+            test_model_score = r2_score(Y_test, Y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
